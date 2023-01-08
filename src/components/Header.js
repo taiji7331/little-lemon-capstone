aa61d7cc532f693078889932eaef675ac4168700
Kyle <taiji7331@gmail.com>
2023-01-08 12:49:55 +0900

    adding basic structure

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="images/littlelemon.png" alt="Little Lemon Logo"></img>
+    </header>
+  );
+};
+export default Header;
