0e4001fe96512f7d9d26d2d83f6bde8774229cd8
Kyle <taiji7331@gmail.com>
2023-02-01 13:43:57 +0900

    changed structure to be more responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,78 +3,56 @@ import Logo from '../images/Logo.svg';
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="row">
-      <img src={Logo} alt="Little Lemon Restaurant" />
-        <div>
-          <h5>Doormat Navigation</h5>
-          <ul>
-            <li>
-              <a href="/home">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/menu">Menu</a>
-            </li>
-            <li>
-              <a href="/reservations">Reservations</a>
-            </li>
-            <li>
-              <a href="/order">Order Online</a>
-            </li>
-            <li>
-              <a href="/login">Login</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h5>Contact</h5>
-          <p>
-            Address
-          </p>
-          <p>
-            Phone number
-          </p>
-          <p>
-            Email
-          </p>
-        </div>
-        <div>
-          <h5>Social Media Links</h5>
-          <ul>
-            <li>
-              <a href="http://google.com/+">
-                Google Plus
+    <footer className="footer">
+      <div className="container">
+        <div className="row justify-content-center">
+          <img className="col-12 col-sm-2" src={Logo} alt="Little Lemon Restaurant" />
+          <div className="col-6 col-sm-4">
+            <h5>Doormat Navigation</h5>
+            <ul className="list-unstyled">
+              <li><a className="nav-link" href="/home">Home</a></li>
+              <li><a className="nav-link" href="/aboutus">About</a></li>
+              <li><a className="nav-link" href="/menu">Menu</a></li>
+              <li><a className="nav-link" href="/contactus">Reservations</a></li>
+              <li><a className="nav-link" href="/contactus">Order Online</a></li>
+            </ul>
+          </div>
+          <div className="col-6 col-sm-6">
+            <h5>Contact</h5>
+            <address>
+              111 Main St.<br />
+              Chicago, Illinois<br />
+              60007 USA<br />
+              <i className="fa fa-phone fa-lg"></i>: +1 (234) 567-8910<br />
+              <i className="fa fa-fax fa-lg"></i>: +1 (234) 567-8911<br />
+              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
+                confusion@food.net
               </a>
-            </li>
-            <li>
-              <a href="http://facebook.com/profile.php?id=">
-                Facebook
+            </address>
+          </div>
+          <div className="col-12 align-self-center">
+            <div className="text-center">
+              <h5>Social Media Links</h5>
+              <a className="btn btn-social-icon btn-google" href="http://google.com/+">
+                <i className="fa fa-google-plus"></i>
               </a>
-            </li>
-            <li>
-              <a href="http://linkedin.com/in">
-                LinkedIn
+              <a className="btn btn-social-icon btn-facebook" href="http://facebook.com/profile.php?id=">
+                <i className="fa fa-facebook"></i>
               </a>
-            </li>
-            <li>
-              <a href="http://twitter.com/">
-                Twitter
+              <a className="btn btn-social-icon btn-linkedin" href="http://linkedin.com/in">
+                <i className="fa fa-linkedin"></i>
               </a>
-            </li>
-            <li>
-              <a href="http://youtube.com">
-                Youtube
+              <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/">
+                <i className="fa fa-twitter"></i>
               </a>
-            </li>
-            <li>
-              <a href="mailto:">
-                Email
+              <a className="btn btn-social-icon btn-google" href="http://youtube.com">
+                <i className="fa fa-youtube"></i>
               </a>
-            </li>
-          </ul>
+              <a className="btn btn-social-icon" href="mailto:">
+                <i className="fa fa-envelope-o"></i>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,114 @@
-import React from "react";
+import React, {Component} from 'react';
 import Logo from '../images/Logo.svg';
+import {Nav, Navbar, NavbarToggler, NavbarBrand, Collapse, NavItem, Button,
+  Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label} from 'reactstrap';
 
-const Header = () => {
-  return (
-    <header>
-      <img src={Logo} alt="Little Lemon Restaurant" />
-    </header>
-  );
-};
+class Header extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: " + this.password.value
+      + " Remember: " + this.remember.checked);
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <Navbar expand='lg'>
+        <NavbarBrand className="mr-auto" href="/">
+          <img src={Logo} alt="Little Lemon Restaurant" />
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggleNav} />
+        <Collapse isOpen={this.state.isNavOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <a className="nav-link" href="/home">
+                <span className="fa fa-home fa-lg"></span> Home
+              </a>
+            </NavItem>
+            <NavItem>
+              <a className="nav-link" href="/about">
+                <span className="fa fa-info fa-lg"></span> About
+              </a>
+            </NavItem>
+            <NavItem>
+              <a className="nav-link" href="/menu">
+                <span className="fa fa-list fa-lg"></span> Menu
+              </a>
+            </NavItem>
+            <NavItem>
+              <a className="nav-link" href="/reservations">
+                <span className="fa fa-bell fa-lg"></span> Reservations
+              </a>
+            </NavItem>
+            <NavItem>
+              <a className="nav-link" href="/order">
+                <span className="fa fa-shopping-bag fa-lg"></span> Order Online
+              </a>
+            </NavItem>
+          </Nav>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <Button outline onClick={this.toggleModal}>
+                <span className="fa fa-sign-in fa-lg"></span> Login
+              </Button>
+            </NavItem>
+          </Nav>
+        </Collapse>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input type="text" id="username" name="username"
+                  innerRef={(input) => this.username = input} />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" name="password"
+                  innerRef={(input) => this.password = input} />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" name="remember"
+                    innerRef={(input) => this.remember = input} />
+                  Remember me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">Login</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </Navbar>
+    );
+  };
+}
+
 export default Header;
